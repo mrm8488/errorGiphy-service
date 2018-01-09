8608c309bd7f9f6f3a52f7811b9b59f838713cd3
Manuel Romero <mrm8488@gmail.com>
2018-01-09 03:20:35 +0100

    Refactor index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 
 const isProduction = process.env.NODE_ENV === "production";
+const port = process.env.PORT || 3000;
 
 /* MIDDLEWARE (stack inspired by kikobeats) */
 
@@ -11,13 +12,21 @@ app
   .use(require("helmet")())
   .use(require("compression")())
   .use(require("cors")())
-  .use(require('morgan')(isProduction ? 'combined' : 'dev'))
+  .use(require("morgan")(isProduction ? "combined" : "dev"))
   .disable("x-powered-by");
 
 require("./routes.js")(app);
 
-app.get("*", (req, res) => res.status(404).send("error: code not found!"));
+app.get("*", (req, res) =>
+  res.format({
+    html: () => res.status(404).send(`Resource not found!`),
 
-app.listen(process.env.PORT || 3000, () =>
-  console.info(`Express server running on port 3000`)
+    json: () =>
+      res.status(404).json({
+        success: false,
+        error: `resource not found`
+      })
+  })
 );
+
+app.listen(port, () => console.info(`Express server running on port ${port}`));
